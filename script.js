c1245666bc8669624408a948cd0a1091db7b8c0c
itsaladdin <aladdin.husseinali@elev.cybergymnasiet.se>
2023-03-17 10:23:18 +0100

    made a new css style

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+// --------------------------------------------------------------------------------
+
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
